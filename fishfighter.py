41e5798c2a9c0b4d01599d64815ce8b6dfb9752a
Chapi123} <schaparro@alumno.huergo.edu.ar>
2026-06-03 09:47:55 -0300

    fish fighting simulator :)

---
+++ fishfighter.py
@@ -0,0 +1,22 @@
+from queue_stack_class import Stack
+
+def surviving_fish(A,B):
+    instance = Stack()
+    survivors = 0
+
+    for i in range(len(A)):
+        
+        if B[i] == 1:
+            instance.push(A[i])
+
+        else:
+            
+            while instance.peek() is not None and A[i] > instance.peek():
+                instance.pop()
+
+            if instance.isEmpty():
+                survivors +=1
+    return survivors + instance.size()
+
+print(surviving_fish([2,5,7,9,2,4], [0,1,0,1,0,0]))
+        
