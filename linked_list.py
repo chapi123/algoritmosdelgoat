c370a213f2710335ee8055a518a2c62f295a006b
Chapi123} <schaparro@alumno.huergo.edu.ar>
2026-06-09 11:44:23 -0300

    double link a la mitad

--- linked_list.py
+++ linked_list.py
@@ -29,8 +29,41 @@ class SingleLink:
         current = self.head
         for _ in range(index):
             current = current.next
+        return current.data
+
+class DoubleLink:
+    
+    def __init__ (self, values):
+        
+        nodes = [Node(value) for value in values]
+
+        for i in range(len(nodes)-1):
+            nodes[i].next = nodes[i+1]
+        
+        for i in range(len(nodes)):
+            if i == 0 : pass
+            else:
+                nodes[i].prev = nodes[i-1]
+
+        self.head = nodes[0]
+        self.tail = nodes[-1]
+
+
+    def print_values (self):
+        current = self.head
+        while current != None:
+            print(current.data)
+            current = current.next
+
+    def iscicled (self):
+        return self.tail == None
+
+    def get_item (self, index):
+        current = self.head
+        for _ in range(index):
+            current = current.next
         return current
 
 
-instance = SingleLink([1,2,4,6,8,0,4])
-print(instance[1].data)
+instance = DoubleLink([1,2,5,7,8,9])
+print(instance.get_item(4).prev.data)
